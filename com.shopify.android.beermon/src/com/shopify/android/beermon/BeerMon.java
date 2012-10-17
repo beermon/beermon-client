6108bf51f375ea361df2ca22abed8575604bbe1d
Chris Saunders <chris.saunders@shopify.com>
2012-10-17 23:38:54 +0000

    woooo

---
+++ com.shopify.android.beermon/src/com/shopify/android/beermon/BeerMon.java
@@ -0,0 +1,15 @@
+package com.shopify.android.beermon;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeerMon extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
