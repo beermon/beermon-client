00f5c39fb1a9938ff52d9b422d997e3a4faa627c
Carson Brown <carson@carsonbrown.ca>
2012-10-18 19:09:02 -0400

    Refactors main activity name, adds beer info to tag and animates it!

--- com.shopify.android.beermon/src/com/shopify/android/beermon/MainActivity.java
+++ com.shopify.android.beermon/src/com/shopify/android/beermon/MainActivity.java
@@ -26,12 +26,14 @@ import java.util.Random;
 import java.util.Arrays;
 import java.util.List;
 
-public class BeerMon extends Activity {
+public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
 
     Tap leftTap, rightTap;
+
+    TapFragment leftTapFragment, rightTapFragment;
 
 
     @Override
@@ -41,24 +43,43 @@ public class BeerMon extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();    //To change body of overridden methods use File | Settings | File Templates.
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         FragmentManager fragmentManager = getFragmentManager();
-        TapFragment tapFragment1 = (TapFragment)fragmentManager.findFragmentById(R.id.tap_1);
-        TapFragment tapFragment2 = (TapFragment)fragmentManager.findFragmentById(R.id.tap_2);
+        leftTapFragment = (TapFragment)fragmentManager.findFragmentById(R.id.tap_1);
+        rightTapFragment = (TapFragment)fragmentManager.findFragmentById(R.id.tap_2);
 
-        SlantedTextView slantedTextView1 = (SlantedTextView)tapFragment1.getView().findViewById(R.id.date);
+        SlantedTextView slantedTextView1 = (SlantedTextView)leftTapFragment.getView().findViewById(R.id.date);
         Random random = new Random();
         slantedTextView1.angle = random.nextInt(6) - 3;
 
-        SlantedTextView slantedTextView2 = (SlantedTextView)tapFragment2.getView().findViewById(R.id.date);
+        SlantedTextView slantedTextView2 = (SlantedTextView)rightTapFragment.getView().findViewById(R.id.date);
         slantedTextView1.angle = random.nextInt(6) - 3;
 
+        updateTaps();
+    }
+
+
+    private void updateTaps() {
         BeermonClient client = new BeermonClient(this);
         client.getTaps(new ListCallback<Tap>() {
             @Override
             public void execute(List<Tap> list) {
                 leftTap = list.get(0);
                 rightTap = list.get(1);
+
+                leftTapFragment.update(leftTap);
+                rightTapFragment.update(rightTap);
             }
         });
     }
--- com.shopify.android.beermon/src/com/shopify/android/beermon/views/TapFragment.java
+++ com.shopify.android.beermon/src/com/shopify/android/beermon/views/TapFragment.java
@@ -2,9 +2,17 @@ package com.shopify.android.beermon.views;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import com.shopify.android.beermon.R;
+import com.shopify.android.beermon.models.Beer;
+import com.shopify.android.beermon.models.Tap;
+
+import java.text.SimpleDateFormat;
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,6 +34,52 @@ public class TapFragment extends Fragment {
         LinearLayout ll = (LinearLayout)inflater.inflate(R.layout.tap_status, container, false);
 
         return ll;
+    }
+
+    public void update(Tap tap) {
+        TextView textView = (TextView)getView().findViewById(R.id.name);
+        textView.setText(tap.keg.beer.name);
+
+        textView = (TextView)getView().findViewById(R.id.brewery);
+        textView.setText(tap.keg.beer.brewery);
+
+        textView = (TextView)getView().findViewById(R.id.ibu);
+        textView.setText("IBUS: " + (int)tap.keg.beer.ibus);
+
+        textView = (TextView)getView().findViewById(R.id.srm);
+        textView.setText("SRM: " + (int)tap.keg.beer.srm);
+
+        textView = (TextView)getView().findViewById(R.id.abv);
+        textView.setText("ABV: " + tap.keg.beer.abv + "%");
+
+        textView = (TextView)getView().findViewById(R.id.date);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd yyyy");
+        textView.setText(simpleDateFormat.format(tap.keg.createdAt).toUpperCase());
+
+        setRating(tap.keg.beer.rating);
+
+        View beerTag = (View)getView().findViewById(R.id.beer_tag);
+        if (beerTag.getVisibility() != View.VISIBLE) {
+            //fade it in
+            beerTag.setAlpha(0.0f);
+            beerTag.setVisibility(View.VISIBLE);
+            beerTag.animate().alpha(1.0f);
+        }
+    }
+
+    private void setRating(float rating) {
+        //round it so we can have 5/5 beers
+        rating = Math.round(rating);
+        View[] views = {
+                getView().findViewById(R.id.star1),
+                getView().findViewById(R.id.star2),
+                getView().findViewById(R.id.star3),
+                getView().findViewById(R.id.star4),
+                getView().findViewById(R.id.star5)};
+
+        for (int i=0; i<views.length; ++i) {
+            views[i].setVisibility((i+1 <= rating) ? View.VISIBLE : View.INVISIBLE);
+        }
     }
 
 }
