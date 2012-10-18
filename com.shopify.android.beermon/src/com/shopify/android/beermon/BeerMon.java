27f47c579f3dd13ce9ac9570c3e81b3d6d6e6769
Carson Brown <carson@carsonbrown.ca>
2012-10-18 10:52:26 -0400

    i don't know anymore

--- com.shopify.android.beermon/src/com/shopify/android/beermon/BeerMon.java
+++ com.shopify.android.beermon/src/com/shopify/android/beermon/BeerMon.java
@@ -2,6 +2,9 @@ package com.shopify.android.beermon;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.TextView;
 
 public class BeerMon extends Activity {
     /**
@@ -11,5 +14,19 @@ public class BeerMon extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        String strScreenDIP = "";
+        strScreenDIP += "The logical density of the display: " + dm.density + "\n";
+        strScreenDIP += "The screen density expressed as dots-per-inch: " + dm.densityDpi +"\n";
+        strScreenDIP += "The absolute height of the display in pixels: " + dm.heightPixels +"\n";
+        strScreenDIP += "The absolute width of the display in pixels: " + dm.widthPixels + "\n";
+        strScreenDIP += "A scaling factor for fonts displayed on the display: " + dm.scaledDensity + "\n";
+        strScreenDIP += "The exact physical pixels per inch of the screen in the X dimension: " + dm.xdpi + "\n";
+        strScreenDIP += "The exact physical pixels per inch of the screen in the Y dimension: " + dm.ydpi + "\n";
+
+        Log.v("CDB", strScreenDIP);
     }
 }
